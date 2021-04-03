0f6a95377b4fec35747d94992c50f1404b9747e6
鄭惠文 <chenghuiwen@zhenghuiwendeMacBook-Pro.local>
2021-04-03 14:32:52 +0800

    init commit src

---
+++ src/main/GUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class GUI {
+  public GUI(){
+    JFrame frame = new JFrame("UML Editor");
+    frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+    frame.setSize(800,800);
+    frame.setVisible(true);
+  }
+  
+  
+}
